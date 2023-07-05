1c3c81302802e8b8b5884f681aeab7675c6c1057
gabriel roberts <gabrielrobertssoares@gmail.com>
2023-07-05 15:12:29 -0300

    Add files via upload

---
+++ Form1.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace INSS
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if(textBox1.Text=="admin" &&  textBox2.Text == "admin")
+            {
+                Form2 form2 = new Form2 ();
+                this.Hide();
+                form2 . ShowDialog ();
+            }
+
+            else
+            {
+                MessageBox.Show(" Login ou Senha Inválidos ! ");
+            }
+        }
+    }
+}
---
+++ Form2.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+
+namespace INSS
+{
+    public partial class Form2 : Form
+    {
+        public Form2()
+        {
+            InitializeComponent();
+        }
+        inss ci = new inss();
+        private void button1_Click(object sender, EventArgs e)
+        {
+            ci.salario = Convert.ToDouble(txtsalario.Text);
+            ci.horasextras = Convert.ToDouble(txthorasextras.Text);
+            ci.Calcularsalbruto();
+            ci.Calcularinss();
+            txtsalariobruto.Text = ci.salariobruto.ToString();
+            txtdesconto.Text = ci.dsctoinss.ToString();
+            txtsalarioliquido.Text = ci.salarioliquido.ToString();
+            txtfuncio.Text = txtfuncionario.Text;
+
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            txtfuncionario.Text = "";
+            txtsalario.Text = "";
+            txthorasextras.Text = "";
+            txtfuncio.Text = "";
+            txtsalariobruto.Text = "";
+            txtdesconto.Text = "";
+            txtsalarioliquido.Text = "";
+        }
+    }
+}
